074dace07f4591ffea743308ec2c8969c4d7b654
KANHAIYA  RAJPUROHIT <kanhaiyalalrajpurohit16@gmail.com>
2026-03-06 18:22:55 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,67 @@
+function toggleMenu(){
+
+let menu=document.getElementById("dropdown");
+
+menu.style.display=menu.style.display==="block"?"none":"block";
+
+}
+
+function showCategory(type){
+
+let gallery=document.getElementById("gallery");
+
+gallery.innerHTML="";
+
+for(let i=1;i<=20;i++){
+
+let img=document.createElement("img");
+
+img.src="https://source.unsplash.com/300x200/?"+type+"&"+i;
+
+img.onclick=openPopup;
+
+gallery.appendChild(img);
+
+}
+
+}
+
+function openPopup(){
+
+document.getElementById("popup").style.display="block";
+
+}
+
+function closePopup(){
+
+document.getElementById("popup").style.display="none";
+
+}
+
+function sendMessage(e){
+
+e.preventDefault();
+
+let fname=document.getElementById("fname").value;
+
+let lname=document.getElementById("lname").value;
+
+let company=document.getElementById("company").value;
+
+let state=document.getElementById("state").value;
+
+let country=document.getElementById("country").value;
+
+let msg=document.getElementById("msg").value;
+
+let text=`Name:${fname} ${lname}
+Company:${company}
+State:${state}
+Country:${country}
+Message:${msg}`;
+
+let url="https://wa.me/919460574256?text="+encodeURIComponent(text);
+
+window.open(url);
+
+}
